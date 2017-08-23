c97e69c5962e5baa6c5f84de24e2102f6cd62a8e
Erin Schnabel <schnabel@us.ibm.com>
2017-08-23 14:05:52 -0400

    remove a8 entirely, dns resolve
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- startup.sh
+++ startup.sh
@@ -5,12 +5,12 @@ if [ ! -z "$ETCD_SECRET" ]; then
   . /data/primordial/setup.etcd.sh /data/primordial $ETCD_SECRET
 fi
 
-export A8_SERVICE=proxy
 
 if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   if [ "$PROXY_CONFIG" == "" ]; then
     PROXY_CONFIG=/etc/haproxy/haproxy.cfg
   fi
+
   echo Setting up etcd...
   echo "** Testing etcd is accessible"
   etcdctl --debug ls
@@ -33,36 +33,17 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   sed -i s/PLACEHOLDER_DOCKERHOST/$(etcdctl get /proxy/docker-host)/g /etc/haproxy/haproxy.cfg
   sed -i s/PLACEHOLDER_LOGHOST/$(etcdctl get /logstash/endpoint)/g /etc/haproxy/haproxy.cfg
 
-  export AUTH_ENDPOINT=$(etcdctl get /endpoints/auth)
-  export ROOM_ENDPOINT=$(etcdctl get /endpoints/room)
-  export MAP_ENDPOINT=$(etcdctl get /endpoints/map)
-  export MEDIATOR_ENDPOINT=$(etcdctl get /endpoints/mediator)
-  export PLAYER_ENDPOINT=$(etcdctl get /endpoints/player)
-  export WEBAPP_ENDPOINT=$(etcdctl get /endpoints/webapp)
-  export SWAGGER_ENDPOINT=$(etcdctl get /endpoints/swagger)
   export SLACKIN_ENDPOINT=$(etcdctl get /endpoints/slackin)
-  export A8_REGISTRY_URL=$(etcdctl get /amalgam8/registryUrl)
-  export A8_CONTROLLER_URL=$(etcdctl get /amalgam8/controllerUrl)
-  export A8_CONTROLLER_POLL=$(etcdctl get /amalgam8/controllerPoll)
-  JWT=$(etcdctl get /amalgam8/jwt)
 
-  sudo service rsyslog start 
-
-  echo Starting haproxy...
-  if [ -z "$A8_REGISTRY_URL" ]; then 
-    #no a8, just run haproxy.
-    haproxy -f $PROXY_CONFIG
-  else
-    #a8, configure security, and run via sidecar.
-    if [ ! -z "$JWT" ]; then     
-      export A8_REGISTRY_TOKEN=$JWT
-      export A8_CONTROLLER_TOKEN=$JWT
-    fi  
-    exec a8sidecar --proxy haproxy -f $PROXY_CONFIG
-  fi  
-  echo HAProxy shut down
+  sudo service rsyslog start
 else
-  echo HAProxy will log to STDOUT--this is dev environment.
+  if [ "$PROXY_CONFIG" == "" ]; then
+    PROXY_CONFIG=/etc/haproxy/haproxy-dev.cfg
+  fi
+
   sed -i s/PLACEHOLDER_PASSWORD/$ADMIN_PASSWORD/g /etc/haproxy/haproxy-dev.cfg
-  exec a8sidecar --proxy haproxy -f /etc/haproxy/haproxy-dev.cfg
 fi
+
+echo Starting haproxy...
+haproxy -f $PROXY_CONFIG
+echo HAProxy shut down
