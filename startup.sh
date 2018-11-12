129b6983a1a30f54836ac8381ce5bd63e82dc5ed
Ozzy Osborne <ozzy@ca.ibm.com>
2018-11-11 19:18:34 -0500

    Restore etcd install, fix etcd test loop

--- startup.sh
+++ startup.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Configure our link to etcd based on shared volume with secret
-if [ ! -z "$ETCD_SECRET" ]; then
-  . /data/primordial/setup.etcd.sh /data/primordial $ETCD_SECRET
-fi
-
 log() {
   if [ "${GAMEON_LOG_FORMAT}" == "json" ]; then
     # This needs to be escaped using jq
@@ -16,21 +11,14 @@ log() {
 
 if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   log Setting up etcd...
-  local RC=1
-  local count=0
+  etcdctl --debug ls
+  RC=$?
   while [ $RC -ne 0 ]; do
-    if [ $count -gt 15 ]; then
-      log "Unable to reach etcd"
-      exit 1
-    fi
-
-    log "** Testing etcd is accessible"
-    etcdctl --debug ls
-    RC=$?
-    if [ $RC -ne 0 ]; then
       sleep 15
-      ((count++))
-    fi
+      # recheck condition
+      log "** Re-testing etcd connection"
+      etcdctl --debug ls
+      RC=$?
   done
   log "etcdctl returned sucessfully, continuing"
 
